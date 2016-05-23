a61603c0c7f566ef9e627c447e1ebce7ddc825f5
RayNieva <raynieva@gmail.com>
2016-05-22 21:03:54 -0400

    modified:   app/controllers/application_controller.rb 	modified:   app/controllers/application_controller.rb~

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello     
-	render text: "hello, world!"
+	render text: "<h1>Hello, world test!</h1><br /> 
+Welcome to Ruby on Rails Using the canonical Hello World! in programming. 
+<p>This process is somewhat different from the others presented as it is deployed by Heroku using the Ruby on Rails Web Application Framework, by directly manipulating the application controller in MVC .</p>
+<p>We will start with a simple application and progress further into more complex applications.</p>
+<p>More to come...</p>	"
+
   end
 
 end
